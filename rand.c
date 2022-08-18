06fc57b336f60a5d629e7ebdc5348d0af9fbf778
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-18 22:11:59 +0530

    Update rand.c

--- rand.c
+++ rand.c
@@ -1,3 +1,27 @@
 // To print bold letters, use  \e[1m at the beginning and \e[m at the end.
 
 //  colors issue link: https://stackoverflow.com/questions/10293839/why-in-rails-cli-do-i-constantly-see-%E2%86%9032m-%E2%86%900m
+
+/*
+#include <stdio.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+int main (int argc, char const *argv[]) {
+
+  printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
+
+  return 0;
+}
+*/
