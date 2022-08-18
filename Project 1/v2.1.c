e1d0b2b536b00d714203d675ff9a426b690a7b0d
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-18 17:19:14 +0530

    v2.2
    Modified Up, Down, Right and left functions to perform better.

--- Project 1/v2.1.c
+++ Project 1/v2.1.c
@@ -4,11 +4,11 @@
 // Header Files
 #include<stdio.h>
 #include<string.h>
-#include<conio.h>
+/*#include<conio.h>*/
 #include<ctype.h>
 
 //Global declaration of some essential variables
-int n=4, m=4, chances=3;
+int n=4, m=4, chances=6;
 
 // Function to Swap the elements passed
 void swap(char* a, char* b){
@@ -20,14 +20,14 @@ void swap(char* a, char* b){
 
 // Function to print the array in required format
 void printarray( char* array[n][m] ){
-    printf("\n-----------------------------\n");
+    printf("\n\t\t\t-----------------------------\n");
     for( int i=0 ; i<n ; i++ ){
         for( int j=0 ; j<n ; j++ ){
             if ( j==0 ){
                 if( strlen(array[i][j]) >1 )
-                     printf( "|  %s  |", array[i][j] );
+                     printf( "\t\t\t|  %s  |", array[i][j] );
                 else
-                     printf( "|   %s  |", array[i][j] );
+                     printf( "\t\t\t|   %s  |", array[i][j] );
             }
             else{
                 if( strlen( array[i][j] )>1 )
@@ -39,71 +39,89 @@ void printarray( char* array[n][m] ){
         }
         printf("\n");
     }
-    printf("-----------------------------\n");
+    printf("\t\t\t-----------------------------\n");
 }
 
 
 // Function to be executed when left command is selected
 void left(char* array[n][m]){
+    int p,q;
     for(int i=0 ; i<n ; i++){
         for(int j=0 ; j<m ; j++){
-            if (array[i][j]==" " && j!=3){
-                swap( &array[i][j], &array[i][j+1]);
+            if(array[i][j]==" "){
+                p=i;q=j;
                 break;
             }
-            else if (array[i][j]==" " && j==3)
-                printf("Invalid Move !");
         }
     }
+    if(q==3)
+        printf("Invalid Move !");
+    else
+        swap(&array[p][q],&array[p][q+1]);
     printarray(array);
 }
 
 // Function to be executed when right command is selected
+
 void right(char* array[n][m]){
+    int p,q;
     for(int i=0 ; i<n ; i++){
         for(int j=0 ; j<m ; j++){
-            if (array[i][j]==" " && j!=0){
-                swap( &array[i][j], &array[i][j-1]);
+            if(array[i][j]==" "){
+                p=i;q=j;
                 break;
             }
-            else if (array[i][j]==" " && j==0)
-                printf("Invalid Move !");
         }
     }
+    if(q==0)
+        printf("Invalid Move !");
+    else
+        swap(&array[p][q],&array[p][q-1]);
     printarray(array);
 }
+
 
 // Function to be executed when up command is selected
+
 void up(char* array[n][m]){
+    int p,q;
     for(int i=0 ; i<n ; i++){
         for(int j=0 ; j<m ; j++){
-            if (array[i][j]==" " && i!=3){
-                swap( &array[i][j], &array[i+1][j]);
+            if(array[i][j]==" "){
+                p=i;q=j;
                 break;
             }
-            else if (array[i][j]==" " && i==3)
-                printf("Invalid Move !");
         }
     }
+    if(p==3)
+        printf("Invalid Move !");
+    else
+        swap(&array[p][q],&array[p+1][q]);
     printarray(array);
 }
 
+
 // Function to be executed when down command is selected
+
 void down(char* array[n][m]){
+    int p,q;
     for(int i=0 ; i<n ; i++){
         for(int j=0 ; j<m ; j++){
-            if (array[i][j]==" " && i!=0){
-                swap( &array[i][j], &array[i-1][j]);
+            if(array[i][j]==" "){
+                p=i;q=j;
                 break;
             }
-            else if (array[i][j]==" " && i==0)
-                printf("Invalid Move !");
         }
     }
+    if(p==0)
+        printf("Invalid Move !");
+    else
+        swap(&array[p][q],&array[p-1][q]);
     printarray(array);
 }
 
 // Function to compare two passed arrays
+
 int compare(char* array[n][m]){
     char *win[4][4] = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14","15"," "}};
     for(int i=0 ; i<n ; i++){
@@ -118,10 +136,10 @@ int compare(char* array[n][m]){
 // Function which contains the switch case for the movements of the numbers in array
 void game(char* name){
     char input;
-    char* array[4][4] = { {"1","2","3","4"}, {"5","6","7","8"}, {"9","10","11","12"}, {" ","13","14","15",} };
+    char* array[4][4] = { {"1","2","3","4"}, {"5"," ","6","7"}, {"9","10","11","8"}, {"13","14","15","12",} };
     printarray(array);
     for(int i=1 ; i<=chances ; chances--){
-        printf("\nHello, %s. There are %d chances remaining.",name,chances);
+        printf("\n\n\t\t  Hello, %s. There are %d chances remaining.\n",name,chances);
         input = _getch();
         if ( input=='w' || input=='W' )
             up(array);
@@ -132,30 +150,34 @@ void game(char* name){
         else if( input=='d' || input=='D' )
             right(array);
         else if( input=='e' || input=='E' ){
-            printf("\n\n----- GAME TERMINATED ON REQUEST -----\n\n");
+            printf("\n\n\t\t----- GAME TERMINATED ON REQUEST -----\n\n");
             return;
         }
         else{
-            printf("\n\n---Invalid Input--- \n\n");
+            printf("\n\n\t\t---Invalid Input--- \n\n");
         }
-    }
+
     // comparing arrays and checking for the game status inside the game function
     if (compare(array)==0 && chances==0){
-        printf("\n\n-----YOU LOSE-----\n\n");
+        printf("\n\n\t\t\t\t-----YOU LOSE-----\n\n");
+        break;
     }
     else if(compare(array)==1){
-        printf("\n\n-----YOU WIN-----\n\n");
+        printf("\n\n\t\t\t\t-----YOU WIN-----\n\n");
+        break;
     }
+}
 }
 
 // Function to print the Rules of the Game
 void printrules(char* name){
-    printf("\n\n----- GAME RULES ------\n\n");
     printf("\nHello, %s\n\n", name);
+    printf("\n\n----- RULES OF THIS GAME------\n\n");
     printf("\n1. You can move only one step at a time.\n");
     printf("\tMove Up: Use Up-arrow key\n");
     printf("\tMove Down: Use Down-arrow key\n");
-    printf("\tMove Right or left: Use right-arrow or left-arrow keys\n");
+    printf("\tMove Right: Use right-arrow Key\n");
+    printf("\tMove Left: Use left-arrow Key\n");
     printf("\n2. You can move the number to an empty position only.\n");
     printf("\n3. For each valid move,your total number of moves will decrease by 1.\n");
     printf("\n4. Winning Situation: Numbers in the 4*4 matrix should be ascending from 1 to 15 and last cell as empty.\n");
@@ -164,8 +186,9 @@ void printrules(char* name){
     printarray(array);
 
     printf("\n5. Press 'e' or 'E' to exit anytime halfway.\n");
-    printf("\n........Good Luck ! Happy Gaming.......\n.");
-    printf("\n\tPress any key to continue...\n");
+    printf("\nSo try to finish the game in minimum number of chances.\n");
+    printf("\n\tGood Luck ! Happy Gaming\t\t\n");
+    printf("\nPress any key to continue...\n");
     _getch();
     game(name);
 }
