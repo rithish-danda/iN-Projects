486c521e2c4605ba87bd76c5be7158509ba0dd76
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-18 22:11:30 +0530

    Update rand.c

--- rand.c
+++ rand.c
@@ -1,3 +1,3 @@
 // To print bold letters, use  \e[1m at the beginning and \e[m at the end.
 
-// 
+//  colors issue link: https://stackoverflow.com/questions/10293839/why-in-rails-cli-do-i-constantly-see-%E2%86%9032m-%E2%86%900m
